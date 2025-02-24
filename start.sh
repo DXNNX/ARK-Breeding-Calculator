67e0e159e2ae47777edcbd4b8722c0f5a72024e7
DXNNX <dchaves@sysco.cr>
2025-02-24 11:41:05 -0600

    Ajuste restart

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Restarting Docker container..." >> /root/ARK-Breeding-Calculator/restart.log
+echo "`date` - Restarting Docker container..." >> /root/ARK-Breeding-Calculator/restart.log
 docker-compose down >> /root/ARK-Breeding-Calculator/restart.log 2>&1
 docker-compose up -d --build >> /root/ARK-Breeding-Calculator/restart.log 2>&1
-echo "Container restarted successfully." >> /root/ARK-Breeding-Calculator/restart.log
+echo "`date` - Container restarted successfully." >> /root/ARK-Breeding-Calculator/restart.log
