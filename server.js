5bea7a819b1f3f559ae5c2f82529e2308e533d30
DXNNX <dchaves@sysco.cr>
2025-02-24 11:38:07 -0600

    Ajuste restar

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
 
+const https = require('https');
+const fs = require('fs');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+const options = {
+    key: fs.readFileSync(process.env.SSL_KEY_PATH),
+    cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+  };
+
+const server = https.createServer(options, app);
+
 
 // Serve static files from the "static" directory
 app.use(express.static(path.join(__dirname, 'static')));
 
 // Start the server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
-sudo docker restart
-sudo docker build . -t breeding
-sudo docker run -it -p 80:3000 breeding
+#!/bin/bash
+
+echo "Restarting Docker container..." >> /root/ARK-Breeding-Calculator/restart.log
+docker-compose down >> /root/ARK-Breeding-Calculator/restart.log 2>&1
+docker-compose up -d --build >> /root/ARK-Breeding-Calculator/restart.log 2>&1
+echo "Container restarted successfully." >> /root/ARK-Breeding-Calculator/restart.log
